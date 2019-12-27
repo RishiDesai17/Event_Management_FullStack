85ec02995b081a8d53c5a945b435f612ded9f9cf
RishiDesai17 <rishindesai@gmail.com>
2019-12-28 03:39:40 +0530

    event model

--- Event_Management_Backend(Node,MongoDB,GraphQL)/app.js
+++ Event_Management_Backend(Node,MongoDB,GraphQL)/app.js
@@ -2,18 +2,40 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphQL = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const Event = require('./models/event');
 
 const app = express();
 app.use(bodyParser.json());
+mongoose.connect("mongodb://localhost:27017/eventsDB",{
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+mongoose.Promise = global.Promise;
 
 app.use('/graphql',graphQL({
     schema: buildSchema(`
+        type Event{
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type Queries{
-            getEvents: [String!]!
+            getEvents: [Event!]!
         }
 
         type Mutations{
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -23,11 +45,28 @@ app.use('/graphql',graphQL({
     `),
     rootValue: {
         getEvents: () => {
-            return ['travel','Music'];
+            return Event.find().exec().then(docs=>{
+                return docs.map(event=>{
+                    return {...event._doc, _id: event.id};
+                })
+            }).catch((err)=>{
+                console.log(err);
+            })
         },
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date(args.eventInput.date) 
+            })
+            return event.save().then((result)=>{
+                console.log(result);
+                return {...result._doc,_id: event.id};
+            }).catch((err)=>{
+                console.log(err);
+                throw err;
+            })
         }
     },
     graphiql: true
